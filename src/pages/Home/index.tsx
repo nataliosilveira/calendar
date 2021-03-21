2d2e1cd672f5769c193e8bb9063e59f2b73bbfba
natalio silveira <nataliosilveira@yahoo.com.br>
2021-03-20 22:41:24 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (<div>
+    <h1>Home Calendar</h1>
+  </div>);
+}
+
+export default Home;
